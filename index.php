ef200f340a01658c65dc1501b8644dd2caaded1d
Elias Ojala <ojala.s.elias@gmail.com>
2017-12-16 18:34:58 +0200

    Created layout with placeholder data

--- index.php
+++ index.php
@@ -20,12 +20,24 @@ if (empty($_GET["host"]) && empty($_GET["ip"])) {
 
     // TODO: Validate $params["host"]
     if(!empty($_GET["host"])) {
-        $params["host"] = $_GET["host"];
+        //$params["host"] = $_GET["host"];
     }
 
     // Validate $params["ip"]
     if(!empty($_GET["ip"]) && filter_var($_GET["ip"], FILTER_VALIDATE_IP) !== false) {
         $params["ip"] = $_GET["ip"];
+        
+        // Some parameters
+        $params["hostname"] = gethostbyaddr($params["ip"]); // TODO: validate
+        $params["asn"] = 15169;
+        $params["isp"] = "Google LLC";
+        $params["city"] = "Mountain View";
+        $params["region"] = "California";
+        $params["country"] = "US";
+
+        // Location
+        $params["lat"] = 37.3860;
+        $params["lon"] = -122.0840;
 
         echo $twig->render('index.html.twig', $params);
     }
