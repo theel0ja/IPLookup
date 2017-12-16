2859cc2e98a9120a7733dbabf4d5589b0075d278
Elias Ojala <ojala.s.elias@gmail.com>
2017-12-16 19:33:26 +0200

    Added real functionality, using ipinfo.io API.

---
+++ functions/getParameters.php
@@ -0,0 +1,49 @@
+<?php
+
+    function ipinfoClient($ip) {
+        $json = file_get_contents("http://ipinfo.io/{$ip}");
+        $details = json_decode($json, true);
+        return $details;
+    }
+
+    /**
+     * Return country name in English for ISO 3166-1 alpha-2 code.
+     * 
+     * @param string $ipAddress
+     * @return array
+     */
+    function getParameters(string $ipAddress) {
+        if(filter_var($ipAddress, FILTER_VALIDATE_IP) !== false) {
+            $params = array();
+            $params["ip"] = $ipAddress;
+
+            $ipinfoData = ipinfoClient($params["ip"]);
+
+            //print_r($ipinfoData);
+
+            //TODO: if($ipinfo["bogon"] === 1) -> SHOW ERROR!
+            
+            // Some parameters
+            //$params["hostname"] = gethostbyaddr($params["ip"]);
+            $params["hostname"] = $ipinfoData["hostname"];
+            $params["asn"] = preg_replace('/\D/', '', explode(" ", $ipinfoData["org"])[0]);
+            $params["isp"] = str_replace(
+                explode(" ", $ipinfoData["org"])[0],
+                "",
+                $ipinfoData["org"]
+            );
+            $params["city"] = $ipinfoData["city"];
+            $params["region"] = $ipinfoData["region"];
+            $params["country"] = $ipinfoData["country"];
+            
+            // Location
+            $params["lat"] = explode(",", $ipinfoData["loc"])[0];
+            $params["lon"] = explode(",", $ipinfoData["loc"])[1];
+
+            return $params;
+        }
+        else {
+            // TODO: throw error
+            return false;
+        }
+    }
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once 'vendor/autoload.php';
 require_once 'functions/getCountryName.php';
+require_once 'functions/getParameters.php';
 
 // Load Twig
 $loader = new Twig_Loader_Filesystem('templates');
@@ -12,33 +13,19 @@ $twig = new Twig_Environment($loader, array(
 
 if (empty($_GET["host"]) && empty($_GET["ip"])) {
     // Show default view
+    $params = getParameters($_SERVER['REMOTE_ADDR']);
 
-    echo $twig->render('index.html.twig');
+    echo $twig->render('index.html.twig', $params);
 } else {
     // Show default view with some parameters
 
-    $params = array();
-
-    // TODO: Validate $params["host"]
     if(!empty($_GET["host"])) {
-        //$params["host"] = $_GET["host"];
+        echo "TODO: Not implemented yet";
     }
 
-    // Validate $params["ip"]
+    // Validate $_GET["ip"]
     if(!empty($_GET["ip"]) && filter_var($_GET["ip"], FILTER_VALIDATE_IP) !== false) {
-        $params["ip"] = $_GET["ip"];
-        
-        // Some parameters
-        $params["hostname"] = gethostbyaddr($params["ip"]); // TODO: validate
-        $params["asn"] = 15169;
-        $params["isp"] = "Google LLC";
-        $params["city"] = "Mountain View";
-        $params["region"] = "California";
-        $params["country"] = "US";
-
-        // Location
-        $params["lat"] = 37.3860;
-        $params["lon"] = -122.0840;
+        $params = getParameters($_GET["ip"]);
 
         echo $twig->render('index.html.twig', $params);
     }
