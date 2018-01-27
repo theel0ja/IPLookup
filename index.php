47d13795eb3df084ac8f72b424e5e85f4b0df68c
Elias Ojala <ojala.s.elias@gmail.com>
2018-01-27 15:07:30 +0200

    Added map navigation controls, added vlucas/phpdotenv, getMapboxKey function, etc.

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
 
+// Libraries
 require_once 'vendor/autoload.php';
+
+// Load vlucas/phpdotenv
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+// Functions
 require_once 'functions/getCountryName.php';
 require_once 'functions/getParameters.php';
+
+function getMapboxKey() {
+
+    $key = getenv("MAPBOX_ACCESS_TOKEN");
+
+    if(empty($key)) {
+        die("Mapbox access token not found");
+    }
+
+    return $key;
+}
 
 // Load Twig
 $loader = new Twig_Loader_Filesystem('templates');
@@ -14,20 +32,20 @@ $twig = new Twig_Environment($loader, array(
 if (empty($_GET["host"]) && empty($_GET["ip"])) {
     // Show default view
     $params = getParameters($_SERVER['REMOTE_ADDR']);
-    $params["mapbox_key"] = getenv("MAPBOX_ACCESS_TOKEN");
+    $params["mapbox_key"] = getMapboxKey();
 
     echo $twig->render('index.html.twig', $params);
 } else {
     // Show default view with some parameters
 
     if(!empty($_GET["host"]) && empty($_GET["ip"])) {
-        echo "TODO: Not implemented yet";
+        die("TODO: Not implemented yet");
     }
 
     // Validate $_GET["ip"]
     if(!empty($_GET["ip"]) && filter_var($_GET["ip"], FILTER_VALIDATE_IP) !== false) {
         $params = getParameters($_GET["ip"]);
-        $params["mapbox_key"] = getenv("MAPBOX_ACCESS_TOKEN");
+        $params["mapbox_key"] = getMapboxKey();
 
         echo $twig->render('index.html.twig', $params);
     }
