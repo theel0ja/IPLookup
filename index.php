f07d5af47641553cdcb0cf8710dfa5699b8b03d7
Elias Ojala <ojala.s.elias@gmail.com>
2018-01-27 19:57:56 +0200

    Implemented route for if ?host query parameter is IP address

--- index.php
+++ index.php
@@ -58,7 +58,20 @@ if (empty($_GET["host"]) && empty($_GET["ip"])) {
     // Show default view with some parameters
 
     if(!empty($_GET["host"]) && empty($_GET["ip"])) {
-        die("TODO: Not implemented yet");
+        if(filter_var($_GET["host"], FILTER_VALIDATE_IP) !== false) {
+            // Query parameter is IP address
+
+            $params = getParameters($_GET["host"]);
+            $params["mapbox_key"] = getMapboxKey();
+            $params["project_name"] = getProjectName();
+            $params["debug_mode"] = getDebugMode();
+            
+            echo $twig->render('index.html.twig', $params);
+        } else {
+            // Query parameter is hostname, do host lookup
+
+            die("TODO: Not implemented yet");
+        }
     }
 
     // Validate $_GET["ip"]
