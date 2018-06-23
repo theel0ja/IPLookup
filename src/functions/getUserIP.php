4ac92726d77cd485b5dfa0c6e6ea79d530f32fe2
Elias Ojala <git@eliasojala.me>
2018-06-23 15:49:55 +0200

    Use HTTP_CF_CONNECTING_IP

--- src/functions/getUserIP.php
+++ src/functions/getUserIP.php
@@ -2,5 +2,7 @@
     function getUserIP() {
         $ipRewrite = new CloudFlare\IpRewrite();
 
-        return $_SERVER["REMOTE_ADDR"];
+        // return $_SERVER["REMOTE_ADDR"];
+        
+        return $_SERVER["HTTP_CF_CONNECTING_IP"];
     }
