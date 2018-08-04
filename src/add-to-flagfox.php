99ad722843e157e3d5edfa1b5f74e16369639d7e
Elias Ojala <git@eliasojala.me>
2018-08-04 18:22:20 +0300

    Add to Flagfox page

---
+++ src/add-to-flagfox.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Add to Flagfox - IPLookup</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha256-LA89z+k9fjgMKQ/kq4OO2Mrf8VltYml/VES+Rg0fh20=" crossorigin="anonymous" />
+
+    <style>
+      #videoPlayer {
+        width: 100%;
+        max-width: 100%;
+        
+        height: 640px;
+      }
+    </style>
+
+  </head>
+
+  <body class="container">
+    <h1>Add to Flagfox</h1>
+
+    <video id="videoPlayer"
+      controls autoplay muted>
+      <source src="https://storage.theel0ja.info/videos/add-iplookup/add-iplookup.mp4"
+        type="video/mp4">
+      <source src="https://storage.theel0ja.info/videos/add-iplookup/add-iplookup.webm"
+        type="video/webm">
+    </video>
+
+    <h2>Template</h2>
+    <pre>https://iplookup.theel0ja.info/?ip={IPaddress}&host={domainName}&addon=flagfox</pre>
+
+    <h2>Screenshot</h2>
+    <img alt="Screenshot"
+      src="https://i.imgur.com/FfNxZuv.png">
+
+    <hr>
+
+    <a href="https://iplookup.theel0ja.info">Back to IPLookup</a>
+
+  </body>
+</html>
